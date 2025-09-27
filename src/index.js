2d02ee4b782aba7b3c76d7f8e82e8e6063fc5f36
mateusapsitumorang <mateusapsitumorang@gmail.com>
2025-09-28 01:10:18 +0700

    Perbaikan halaman putih dengan basename dan 404.html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from '../src/main-component/App/App';
 import reportWebVitals from './reportWebVitals';
 import './css/font-awesome.min.css';
@@ -9,13 +10,11 @@ import './css/animate.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './sass/style.scss';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter basename="/portfolio-mateus">
     <App />
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
